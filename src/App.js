5ad5c551c83a1d05f778461096955841a91a4907
Imraan-Hendricks <imraan.hndx@gmail.com>
2020-08-12 00:54:49 +0200

    added footer section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NavbarContextProvider from './components/navbar/navbar-context';
 import Main from './sections';
 import NotFound from './sections/not-found/not-found';
+import Footer from './sections/footer/footer';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route component={Main} exact path='/' strict />
           <Route component={NotFound} />
         </Switch>
+        <Footer />
       </BrowserRouter>
     </NavbarContextProvider>
   );
---
+++ src/sections/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import cssModule from './footer.module.scss';
+
+const Footer = () => {
+  return (
+    <div className={cssModule.root}>
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
